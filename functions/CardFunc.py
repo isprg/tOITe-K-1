c5d6b8b050c5ff1868971428933e3bf18eebec5c
broccolingual <broccolingual@gmail.com>
2022-09-01 13:10:27 +0900

    fix: hotfix

--- functions/CardFunc.py
+++ functions/CardFunc.py
@@ -22,9 +22,9 @@ def SetGame_FromCard(dictArgument):
         dictArgument["Start time"] = sStartTime
 
     # For test
-    # elif dictSaveData["voice"] != "T":
-    #     sStartTime = cState.updateState("FINAL_0")
-    #     dictArgument["Start time"] = sStartTime
+    elif dictSaveData["voice"] != "T":
+        sStartTime = cState.updateState("FINAL_0")
+        dictArgument["Start time"] = sStartTime
 
     elif CheckEnding(cCtrlCard):
         sStartTime = cState.updateState("FINAL_0")
