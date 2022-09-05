adcd1cda9b07d099eda5cdbff7db3f65aacf2a30
broccolingual <broccolingual@gmail.com>
2022-09-05 15:43:29 +0900

    fix: hotfix

--- functions/ModeFuncSR.py
+++ functions/ModeFuncSR.py
@@ -79,6 +79,7 @@ def procSR_Q(dictArgument):
             cAudio.startRecordThread()
         elif sTappedArea == 1 and cAudio.getRecording() == True:
             print("stop recording")
+            PlaySound("sound/button1.wav")
             cAudio.setRecording(False)
             cAudio.record("test.wav")
 
