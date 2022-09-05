c0ecad88ab27521eec14c53c1e5decacbcc83517
Broccolingual <broccolingual@gmail.com>
2022-09-06 00:57:58 +0900

    update: update ClsAudioSensor

--- Classes/ClsAudioSensor.py
+++ Classes/ClsAudioSensor.py
@@ -20,29 +20,33 @@ class ClsAudioSensor:
             with noalsaerr():
                 self.audio = pyaudio.PyAudio()
 
-        self.stream = self.audio.open(
-            format=pyaudio.paInt16,
-            channels=sChannels,
-            rate=sRate,
-            input=True,
-            frames_per_buffer=sUnitSample)
-
     def __del__(self):
         self.finalize()
 
     def finalize(self):
-        self.stream.stop_stream()
-        self.stream.close()
         self.audio.terminate()
 
+    def initBuffer(self):
+        self.vAudio = []
+
     def startRecordThread(self):
+        self.stream = self.audio.open(
+            format=pyaudio.paInt16,
+            channels=self.sChannels,
+            rate=self.sRate,
+            input=True,
+            frames_per_buffer=self.sUnitSample)
+            
         self.vAudio = []
         self.blRecording = True
+        print("start thread")
         self.executor = ThreadPoolExecutor(max_workers=1)
         self.executor.submit(self.sample)
 
     def shutdownRecordThread(self):
         self.executor.shutdown()
+        self.stream.stop_stream()
+        self.stream.close()
 
     def setRecording(self, blRecording):
         self.blRecording = blRecording
@@ -52,6 +56,7 @@ class ClsAudioSensor:
 
     def sample(self):
         while self.blRecording:
+            print("sampling")
             vData = self.stream.read(self.sUnitSample)
             self.vAudio.append(vData)
         self.shutdownRecordThread()
